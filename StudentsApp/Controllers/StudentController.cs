515069a42aa8f6c75d2866aa991b69bdeb04c152
irimia.mihai91 <irimia.mihai91@gmail.com>
2019-02-10 18:12:39 +0200

    sa

---
+++ StudentsApp/Controllers/StudentController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentsApp.Models;
+
+namespace StudentsApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var students = GetList();
+
+            return View(students);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var students = GetList();
+            var student = FindStudent(students, id);
+            return View(student);
+        }
+
+        [HttpDelete]
+        public ActionResult Detele( int id)
+        {
+            var students = GetList();
+            DeleteById(students, id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //var students = GetList();
+            //var student = students.Where(s => s.StudentId == id).FirstOrDefault();
+            //return View(student);
+
+            var students = GetList();
+            var student = FindStudent(students, id);
+            return View(student);
+        }
+
+        [NonAction]
+        public List<Student> GetList()
+        {
+            Student s1 = new Student(1, "Popescu", 22);
+            Student s2 = new Student(2, "Ionescu", 26);
+            Student s3 = new Student(3, "Marcel", 23);
+            Student s4 = new Student(4, "Vaile", 24);
+            Student s5 = new Student(5, "Ion", 22);
+            Student s6 = new Student(6, "Gheorghe", 26);
+            Student s7 = new Student(7, "Daniel", 23);
+            Student s8 = new Student(8, "Popoi", 24);
+
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            students.Add(s3);
+            students.Add(s4);
+            students.Add(s5);
+            students.Add(s6);
+            students.Add(s7);
+            students.Add(s8);
+
+            return students;
+        }
+
+        [NonAction]
+        public Student FindStudent(List<Student> students, int id)
+        {
+            foreach(var stud in students)
+            {
+                if (stud.StudentId == id)
+                {
+                    return stud;
+                }
+            }
+            return null;
+        }
+
+        [NonAction]
+        public void DeleteById(List<Student> students, int id)
+        {
+            foreach(var stud in students)
+            {
+                if (stud.StudentId == id)
+                {
+                    students.Remove(stud);
+                }
+
+            }
+        }
+    }
+}
---
+++ StudentsApp/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsApp.Models
+{
+    public class Student
+    {
+        public Student(int Id, string studentName, int age)
+        {
+            this.StudentId = Id;
+            this.StudentName = studentName;
+            this.Age = age;
+
+        }
+        public int StudentId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
